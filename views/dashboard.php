9f7fdee45de4f84614cadd0596ff3bbad8b6c2c7
Rahul Dahal <rdaahal@gmail.com>
2022-07-25 17:17:31 +0545

    wip: add dashboard view

---
+++ views/dashboard.php
@@ -0,0 +1,41 @@
+<?php
+include(dirname(__DIR__)."/includes/header.php");
+?>
+
+<title>Dashboard</title>
+</head>
+<body>
+<h1>Patient name's Dashboard</h1>
+
+<div class="tabs">
+  <div class="tab-2">
+    <label for="tab2-1">Appointments</label>
+    <input id="tab2-1" name="tabs-two" type="radio" checked="checked">
+    <div>
+      <h4>Appointments you've booked</h4>
+        <ul>
+            <li>Appointment One</li>
+            <li>Appontment Two</li>
+            <li>Appointment Three</li>
+        </ul>
+    </div>
+  </div>
+  <div class="tab-2">
+    <label for="tab2-2">New</label>
+    <input id="tab2-2" name="tabs-two" type="radio">
+    <div>
+      <h4>Book a new appointment</h4>
+      <form action="">
+          <label for="reason">Reason</label>
+          <textarea name="reason" id="reason" cols="30" rows="10"></textarea>
+          
+          <label for="doctor">Doctor</label>
+          <select name="doctor" id="doctor">
+              <option value="DoctorOne">Doctor One</option>
+              <option value="DoctorTwo">Doctor Two</option>
+              <option value="DoctorThree">Doctor Three</option>
+          </select>
+      </form>
+    </div>
+  </div>
+</div>
