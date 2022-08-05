db082269c7b6671835f0abdb97c70b6c2385342f
Nikita Khanal <nikitakhanal30@gmail.com>
2022-08-05 20:50:27 +0545

    admin dashboard

--- views/adminDashboard.php
+++ views/adminDashboard.php
@@ -1,8 +1,9 @@
 <?php
+include(dirname(__DIR__).'/includes/connection.php');
 include(dirname(__DIR__)."/includes/header.php");
 session_start();
 if(count($_SESSION)==0){
-  header('Location: /dabs/index.php');
+  header('Location: /dabs/admin.php');
   exit();
 }
 ?>
@@ -10,17 +11,67 @@ if(count($_SESSION)==0){
 <title>Admin Dashboard</title>
 </head>
 <body>
-<h1><?php echo $_SESSION['firstName']."'s"; ?> Dashboard</h1>
 
 <nav>
-    <li>
-        <a href="addDoctor.php">Add Doctor</a>
-    </li>
-    <li>
-        <a href="../admin/viewDoctors.php">View Doctor</a>
-    </li>
-    <li>
-        <a href="../admin/viewAppointment.php">Appointment Details</a>
-    </li>
-
+  <a href="../logout.php">Log Out</a>
+  <a href="#profile">Profile</a>  
+  <a href="../admin/viewDoctors.php">View Doctor</a>
+  <a href="addDoctor.php">Add Doctor</a>
 </nav>
+
+
+<h1>Admin's Dashboard</h1>
+
+<h2 class="tableTitle">Appointments</h2>
+    <table>
+        <tr>
+            <th>UserId</th>
+            <th>DoctorId</th>
+            <th>AppointmentId</th>
+            <th>Reason</th>
+            <th>Date</th>
+            <th>Time</th>
+            <th>Token</th>
+            <th>Status</th> 
+            <th>Action</th>
+        </tr>
+
+<?php
+$sql = "SELECT * FROM appointment;";
+$resultSet = mysqli_query($conn, $sql);
+$numRows = mysqli_num_rows($resultSet);
+$appointmentId;
+$appointmentStatus;
+if($numRows > 0){
+    $data= array();
+    while($row = mysqli_fetch_assoc($resultSet)){
+        // array_push($data, $row);
+        $appointmentId = $row['appointmentId'];
+        $appointmentStatus = $row['status'];
+        echo "<tr>";
+        // echo "<td>".$row['doctor_id']."</td>";
+        echo "<td>".$row['userId']."</td>";
+        echo "<td>".$row['doctorId']."</td>";
+        echo "<td>".$row['appointmentId']."</td>";
+        echo "<td>".$row['reason']."</td>";
+        echo "<td>".$row['date']."</td>";
+        echo "<td>".$row['timeSlot']."</td>";
+        echo "<td>".$row['token']."</td>";
+        echo "<td>".$row['status']."</td>"; 
+        
+        // echo "<td>".$row['fee']."</td>";
+        echo "<td> 
+            <form action=\"updateAppointmentStatus.php\">
+             <input type='hidden' name='appointmentId' value='$appointmentId' >
+             <input type='hidden' name='appointmentStatus' value='$appointmentStatus' >
+             <button type='submit' name='submit' value='approve' style='color:#000;'>Approve</button>
+            <button type='submit' name='submit' value='decline' style='color:#000;'>Decline</button>
+            </form>
+            </td>";	
+        // <input type=\"submit\" name=\"doctorUpdateDetails\" value=\"Update\" />	
+		echo "</tr>";
+    }
+}
+?>
+
+</table>
