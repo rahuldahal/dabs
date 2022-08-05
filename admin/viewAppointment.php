cc6846adf6319565f94e82dd14051d5950061a9b
Nikita Khanal <nikitakhanal30@gmail.com>
2022-08-05 20:44:01 +0545

    wip: display appointment in descending

--- admin/viewAppointment.php
+++ admin/viewAppointment.php
@@ -1,5 +1,11 @@
 <?php
 include(dirname(__DIR__).'/includes/connection.php');
+include(dirname(__DIR__)."/includes/header.php");
+session_start();
+if(count($_SESSION)==0){
+    header('Location: /dabs/admin.php');
+    exit();
+  }
 ?>
 
 <html>
@@ -7,34 +13,49 @@ include(dirname(__DIR__).'/includes/connection.php');
         <tr>
             <th>UserId</th>
             <th>DoctorId</th>
+            <th>AppointmentId</th>
             <th>Reason</th>
             <th>Date</th>
             <th>Time</th>
             <th>Token</th>
+            <th>Status</th> 
             <th>Action</th>
         </tr>
 
 <?php
-$sql = "SELECT * FROM appointment;";
+$sql = "SELECT * FROM appointment ORDER BY date DESC;";
+// $sql = "SELECT * FROM appointment;";
 $resultSet = mysqli_query($conn, $sql);
 $numRows = mysqli_num_rows($resultSet);
+$appointmentId;
+$appointmentStatus;
 if($numRows > 0){
     $data= array();
     while($row = mysqli_fetch_assoc($resultSet)){
         // array_push($data, $row);
+        $appointmentId = $row['appointmentId'];
+        $appointmentStatus = $row['status'];
         echo "<tr>";
-			// echo "<td>".$row['doctor_id']."</td>";
-			echo "<td>".$row['userId']."</td>";
-								
-			echo "<td>".$row['doctorId']."</td>";
-			echo "<td>".$row['reason']."</td>";
-			echo "<td>".$row['date']."</td>";
-			echo "<td>".$row['time']."</td>";
-            echo "<td>".$row['token']."</td>";
-								
-			// echo "<td>".$row['fee']."</td>";
-            echo "<td><button type='submit' name='submit' style='color:#000;'>Approve</button>
-            <button type='submit' name='submit' style='color:#000;'>Decline</button></td>";		
+        // echo "<td>".$row['doctor_id']."</td>";
+        echo "<td>".$row['userId']."</td>";
+        echo "<td>".$row['doctorId']."</td>";
+        echo "<td>".$row['appointmentId']."<td/>";
+        echo "<td>".$row['reason']."</td>";
+        echo "<td>".$row['date']."</td>";
+        echo "<td>".$row['time']."</td>";
+        echo "<td>".$row['token']."</td>";
+        echo "<td>".$row['status']."</td>"; 
+        
+        // echo "<td>".$row['fee']."</td>";
+        echo "<td> 
+            <form action=\"updateAppointmentStatus.php\">
+             <input type='hidden' name='appointmentId' value='$appointmentId' >
+             <input type='hidden' name='appointmentStatus' value='$appointmentStatus' >
+             <button type='submit' name='submit' value='approve' style='color:#000;'>Approve</button>
+            <button type='submit' name='submit' value='decline' style='color:#000;'>Decline</button>
+            </form>
+            </td>";	
+        // <input type=\"submit\" name=\"doctorUpdateDetails\" value=\"Update\" />	
 		echo "</tr>";
     }
 }
