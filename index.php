ed992606156b71ff6d4b46ec18a0a76a00ad4d08
Rahul Dahal <rdaahal@gmail.com>
2022-09-13 16:34:27 +0545

    refactor: index page and variables

--- index.php
+++ index.php
@@ -1,106 +1,27 @@
 <?php
-include(dirname(__DIR__).'/dabs/includes/connection.php');
+include('./includes/connection.php');
+include('./includes/header.php');
 ?>
 
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Doctor's Appoint Booking Management System</title>
-	<link rel="stylesheet" type="text/css" href="css/styles.css">
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300,500,700&display=swap" rel="stylesheet">
 </head>
 <body>
-	<div class="main">  	
-		<input type="checkbox" id="chk" aria-hidden="true">
+<main class="wrapper">
+  <section class="cover">
+    <div class="cover-left">
+      <h1 class="tagline"><span class="opacity-8">Koshi Zonal Hospital's</span> Doctor Appointment Booking Management System</h1>
+      <p class="subtitle">Making the appointment booking process quick, efficient and hassle free.</p>
+      <a href="views/patientSignup.php" class="CTA">Sign up</a>
+    </div>
+    <div class="cover-img-container"><img class="cover-img" src="http://koshihospital.gov.np/wp-content/uploads/2020/04/koshi-hospital-covid-19-special-hospital.jpg" alt="Koshi Zonal Hospital - New Department"></div>
+  </section>
+  
+</main>
+<footer>
+  <p class="copyright">Copyright @ Koshi Zonal Hospital <span id="year"></span> </p>
+</footer>
 
-			<div class="signin">
-				<form action="auth/patientSignin.php" method="POST">
-					<label class="signinTitle" for="chk" aria-hidden="true">Sign In</label>
-					<div class="fields">
-                        <label for="email">Email</label>
-                        <input type="email" id="email" name="email" placeholder="name@domain.com" required>
-                        <label for="password">Password</label>
-                        <input type="password" id="password" name="password" placeholder="password" required>
-                        <input type="submit" name="patientSigninDetails" value="Sign In" />
-                        Not registered yet? 
-                        <a href="signup.php" id="signup" style="text-decoration: none; color:blanchedalmond;">Sign Up</a>
-                    </div>
-				</form>
-			</div>
-    
-    <div class="signup">
-				<form action="auth/patientSignup.php" method="POST">
-					<label class="signupTitle" for="chk" aria-hidden="true">Sign Up</label>
-
-					<div class="fields multiStepForm">
-                        <div class="steps">
-                        <div data-step="one">
-                            <label for="firstName">First Name</label>
-                            <input type="text" id="firstName" name="firstName" placeholder="Ram" required />
-                            
-                            <label for="middleName">Middle Name</label>
-                            <input type="text" id="middleName" name="middleName" placeholder="Kumar" required />
-
-                            <label for="lastName">Last Name</label>
-                            <input type="text" id="lastName" name="lastName" placeholder="Thapa" required />
-                        </div>
-
-                        <div data-step="two">
-                            <label for="gender">Gender</label>
-                            <select name="gender" id="gender">
-                                <option value="male">Male</option>
-                                <option value="female">Female</option>
-                                <option value="others">Others</option>
-                            </select>
-
-                            <label for="dob">Date of Birth</label>
-                            <input type="date" name="dob" id="dob" />
-
-                            <label for="bloodGroup">Blood Group</label>
-                            <select name="bloodGroup" id="bloodGroup">
-                                <option value="A+">A+</option>
-                                <option value="A-">A-</option>
-                                <option value="B+">B+</option>
-                                <option value="B-">B-</option>
-                                <option value="O+">O+</option>
-                                <option value="O-">O-</option>
-                                <option value="AB+">AB+</option>
-                                <option value="AB-">AB-</option>
-                            </select>
-
-                            <label for="maritalStatus">Marital Status</label>
-                            <select name="maritalStatus" id="maritalStatus">
-                                <option value="single">single</option>
-                                <option value="married">married</option>
-                                <option value="divorced">divorced</option>
-                            </select>
-                        </div>
-
-                        <div data-step="three">
-                            <label for="email">Email</label>
-                            <input type="email" id="email" name="email" placeholder="name@domain.com" required>
-
-                            <label for="password">Password</label>
-                            <input type="password" id="password" name="password" placeholder="password" required />
-                        </div>
-
-                        <div data-step="four">
-                            <label for="address">Address</label>
-                            <input type="address" id="address" name="address" placeholder="Biratnagar" required>
-
-                            <label for="telephone">telephone</label>
-                            <input type="tel" id="telephone" name="telephone" placeholder="telephone" required />
-                        </div>
-                        </div>
-
-                        <button hidden data-button-action="previous">Previous</button>
-                        <button hidden data-button-action="next">Next</button>
-
-                        <!-- <input hidden type="submit" name="patientSignupDetails" value="Sign Up" /> -->
-                        <input type="submit" name="patientSignupDetails" value="Sign Up" />
-                    </div>
-				</form>
-			</div>
-	</div>
+<script>
+    document.querySelector("#year").textContent = new Date().getFullYear();
+</script>
 </body>
 </html>
